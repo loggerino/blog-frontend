4d89ff21112dc3e2faa1ef15e482b572f18de876
Chyisiang <chyisiang8@gmail.com>
2023-09-01 11:51:00 +0800

    init, testing out api from frontend

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://blogapi-nine.vercel.app/api/posts/all')
+      .then(response => response.json())
+      .then(data => setPosts(data))
+      .catch(error => console.error('Error fetching posts:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Blog Posts</h1>
+      <ol>
+        {posts.map(post => (
+          <>
+            <li key={post._id}>{post.title}</li>
+            <p key={post._id}>{post.content}</p>
+          </>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default App;
