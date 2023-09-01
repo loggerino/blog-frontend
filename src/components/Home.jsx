903a4610c61a98594c9cf8d864d96afad9ac654d
Chyisiang <chyisiang8@gmail.com>
2023-09-01 20:51:18 +0800

    home page loading spinner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,35 @@ import PostCard from './PostCard';
 
 function Home() {
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch('https://blogapi-nine.vercel.app/api/posts/all')
             .then(response => response.json())
-            .then(data => setPosts(data))
-            .catch(error => console.error('Error fetching posts:', error));
+            .then(data => {
+                setPosts(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching posts:', error);
+                setLoading(false);
+            });
     }, []);
+
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <div
+                    className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                    role="status"
+                >
+                    <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                        Loading...
+                    </span>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
