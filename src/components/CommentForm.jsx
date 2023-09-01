cfee4a200044a403bb85a1b18c201e9484881aba
Chyisiang <chyisiang8@gmail.com>
2023-09-01 20:40:18 +0800

    polished comment section

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -5,19 +5,19 @@ function CommentForm() {
 
     const handleSubmit = event => {
         event.preventDefault();
-        // Perform POST request to create comment
     };
 
     return (
-        <div>
-            <h3>Add Comment</h3>
-            <form onSubmit={handleSubmit}>
+        <div className="mt-6">
+            <h3 className="text-lg font-medium text-gray-900">Add Comment</h3>
+            <form onSubmit={handleSubmit} className="mt-4">
                 <textarea
                     placeholder="Write your comment..."
                     value={content}
                     onChange={e => setContent(e.target.value)}
+                    className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 />
-                <button type="submit">Submit</button>
+                <button type="submit" className="mt-2 bg-indigo-600 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Submit</button>
             </form>
         </div>
     );
--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 function CommentSection({ comments }) {
     return (
         <div className="comment-section">
-            <h3>Comments</h3>
-            <ul>
-                {comments.map(comment => (
-                    <li key={comment._id}>{comment.content}</li>
-                ))}
+            <h3 className="text-lg font-medium text-gray-900">Comments</h3>
+            <ul className="mt-4 space-y-4">
+                {comments.map(comment => {
+                    const date = new Date(comment.timestamp).toLocaleDateString();
+                    return (
+                        <li key={comment._id} className="bg-gray-50 rounded-md p-4">
+                            <p className="text-sm text-gray-700">{comment.content}</p>
+                            <div className="mt-2 text-xs text-gray-500">{date}</div>
+                        </li>
+                    );
+                })}
             </ul>
         </div>
     );
